654c668c944aaa862936c9912b1ab3ac653c0ced
Caleb Fringer <cfringer98@gmail.com>
2024-11-30 18:55:09 -0800

    Realized I could use 'into' instead of 'concat'

--- day1/solution.clj
+++ day1/solution.clj
@@ -1,14 +1,14 @@
-(ns aoc2022.day1
+(ns aoc2022.day1.solution
   (:require [clojure.string :as str])
   (:import (java.io File)))
 
-(def elves 
-      (map #(str/split % #"\n")
-           (str/split (slurp "input.txt") #"\n\n")))
+(def elves
+  (map #(str/split % #"\n")
+       (str/split (slurp "input.txt") #"\n\n")))
 
 (def snacks (map #(reduce + (map Integer/parseInt %)) elves))
 
-(defn nth-largest 
+(defn nth-largest
   ; Initial call
   ([n numbers] (nth-largest n numbers nil))
   ; Recursive helper
@@ -18,9 +18,9 @@
      (let [maximum (apply max numbers)
            freq_map (frequencies numbers)
            freq_max (get freq_map maximum)]
-       (recur 
-         (max (- n freq_max) 0)
-         (filter #(not (= maximum %)) numbers)
-         (concat result (repeat (min freq_max n) maximum)))))))
+       (recur
+        (max (- n freq_max) 0)
+        (filter #(not (= maximum %)) numbers)
+        (into result (repeat (min freq_max n) maximum)))))))
 
 (apply + (nth-largest 3 snacks))
